8cf6aeeb88d08d4dbacb6d4179d27f1f8ab28d40
Alexteplov <teplov0@gmail.com>
2012-07-28 09:44:55 +0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmanager::Application.config.secret_token = '3fdf1998950415ebc02ff1740e60b66c89c6b90e2d848f1a003ac1605116c27b55035a429bca0a607cffcffce02351c1ef1badabd769bd3aeb7ece3efdf06d9c'
